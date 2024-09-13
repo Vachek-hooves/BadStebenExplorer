b8869d1eab7af599b32fbef037731c8d289d01ef
Vachek-hooves <vachek0602@gmail.com>
2024-09-13 20:08:21 +0300

    updates

--- components/Layout/LayoutImage.js
+++ components/Layout/LayoutImage.js
@@ -1,11 +1,17 @@
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+} from 'react-native';
 
 const LayoutImage = ({children, style}) => {
   return (
     <ImageBackground
-      style={[{flex: 1}, style]}
+      style={[{flex: 1}]}
       source={require('../../assets/img/bg/city.jpg')}>
-      {children}
+      <SafeAreaView style={[{flex: 1}, style]}>{children}</SafeAreaView>
     </ImageBackground>
   );
 };
