aea556779c3f3bbfb2ee39cadcc7cdd54190fee0
Vachek-hooves <vachek0602@gmail.com>
2024-09-13 17:21:28 +0300

    update context.js

--- store/context.js
+++ store/context.js
@@ -1,11 +1,49 @@
 import {useState, useContext, createContext, useEffect} from 'react';
+import {STEBEN_QUIZ, STEBEN_TRUE_FALSE} from '../data/stabenData';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const AppContext = createContext({});
+export const AppContext = createContext({
+  quizData: [],
+  trueFalseData: [],
+});
 
 export const AppProvider = ({children}) => {
-  const [cityQuiz, setCityQuiz] = useState([]);
-  const [trueFalse, setTrueFalse] = useState([]);
-  const value = {};
+  const [quizData, setQuizData] = useState([]);
+  const [trueFalseData, setTrueFalseData] = useState([]);
+
+  useEffect(() => {
+    initializeData();
+  }, []);
+
+  const initializeData = async () => {
+    try {
+      // Check if data already exists in AsyncStorage
+      const storedQuizData = await AsyncStorage.getItem('quizData');
+      const storedTrueFalseData = await AsyncStorage.getItem('trueFalseData');
+
+      if (!storedQuizData) {
+        // If not, store the initial data
+        await AsyncStorage.setItem('quizData', JSON.stringify(STEBEN_QUIZ));
+        setQuizData(STEBEN_QUIZ);
+      } else {
+        setQuizData(JSON.parse(storedQuizData));
+      }
+
+      if (!storedTrueFalseData) {
+        await AsyncStorage.setItem(
+          'trueFalseData',
+          JSON.stringify(STEBEN_TRUE_FALSE),
+        );
+        setTrueFalseData(STEBEN_TRUE_FALSE);
+      } else {
+        setTrueFalseData(JSON.parse(storedTrueFalseData));
+      }
+    } catch (error) {
+      console.error('Error initializing data:', error);
+    }
+  };
+
+  const value = {quizData, trueFalseData};
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
